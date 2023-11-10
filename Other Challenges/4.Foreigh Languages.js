25ba419caec130d26fa6329f8b5ef36e3f06ed9a
Asparuh Bambov <akbamboff@gmail.com>
2023-11-10 10:18:40 +0200

    task 4.

--- Other Challenges/4.Foreigh Languages.js
+++ Other Challenges/4.Foreigh Languages.js
@@ -1,15 +1,41 @@
+// using if, else if
+
+// function countryLanguage(country) {
+//   if (country === "USA" || country === "England") {
+//     console.log(`English`);
+//   } else if (
+//     country === "Spain" ||
+//     country === "Argentina" ||
+//     country === "Mexico"
+//   ) {
+//     console.log(`Spanish`);
+//   } else {
+//     console.log("unknown");
+//   }
+// }
+
+// using switch cases.
 function countryLanguage(country) {
-  if (country === "USA" || country === "England") {
-    console.log(`English`);
-  } else if (
-    country === "Spain" ||
-    country === "Argentina" ||
-    country === "Mexico"
-  ) {
-    console.log(`Spanish`);
-  } else {
-    console.log("unknown");
+  switch (country) {
+    case "USA":
+      console.log("English");
+      break;
+    case "England":
+      console.log("English");
+      break;
+    case "Argentina":
+      console.log("Spanish");
+      break;
+    case "Spain":
+      console.log("Spanish");
+      break;
+    case "Mexico":
+      console.log("Spanish");
+      break;
+    default:
+      console.log("unknown");
   }
 }
 
-countryLanguage("England");
+
+countryLanguage("Bulgaria");
