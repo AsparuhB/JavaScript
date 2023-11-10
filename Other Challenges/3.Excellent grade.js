a052b010e037360e5d823b8e4ee1802352387944
Asparuh Bambov <akbamboff@gmail.com>
2023-11-10 10:13:31 +0200

    sdsd

--- Other Challenges/3.Excellent grade.js
+++ Other Challenges/3.Excellent grade.js
@@ -1,9 +1,9 @@
 function excellentGrade(grade) {
-  if (grade >= 5.50) {
-    console.log("Excellent")
+  if (grade >= 5.5) {
+    console.log("Excellent");
   } else {
-    console.log("Not excellent")
+    console.log("Not excellent");
   }
 }
 
-excellentGrade(5.50)
+excellentGrade(5.5);
