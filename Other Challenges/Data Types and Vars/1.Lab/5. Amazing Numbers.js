df2ada5b9ff8f8fbb95a2eb0d890866bc178b4ea
Asparuh Bambov <akbamboff@gmail.com>
2023-11-15 10:53:58 +0200

    Task finished

--- Other Challenges/Data Types and Vars/1.Lab/5. Amazing Numbers.js
+++ Other Challenges/Data Types and Vars/1.Lab/5. Amazing Numbers.js
@@ -1,1 +1,13 @@
-  
+function amazingNumbers(num) {
+  num = num.toString();
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum += Number(num[i]);
+  }
+
+  const result = sum.toString().includes("9");
+
+  console.log(result ? `${num} Amazing? True` : `${num} Amazing? False`);
+}
+
+amazingNumbers(999)
