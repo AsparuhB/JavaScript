51997c84aa90ba9f8d6a9971f2577599f9cd734c
Asparuh Bambov <akbamboff@gmail.com>
2023-11-15 12:37:15 +0200

    remake...

--- Other Challenges/Fundamentals/2.Data Types and Vars/1.Lab/5. Amazing Numbers.js
+++ Other Challenges/Fundamentals/2.Data Types and Vars/1.Lab/5. Amazing Numbers.js
@@ -1,13 +1,14 @@
 function amazingNumbers(num) {
   num = num.toString();
   let sum = 0;
+
   for (let i = 0; i < num.length; i++) {
     sum += Number(num[i]);
+
   }
-
-  const result = sum.toString().includes("9");
-
+  let result = sum.toString().includes("9");
+  
   console.log(result ? `${num} Amazing? True` : `${num} Amazing? False`);
 }
 
-amazingNumbers(999)
+amazingNumbers(145);
