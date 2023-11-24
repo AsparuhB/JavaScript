e747db3f7af1bb96e8d76ec012b43d031ee94436
Asparuh Bambov <akbamboff@gmail.com>
2023-11-24 16:39:55 +0200

    big sad....

--- Other Challenges/Fundamentals/2.Data Types and Vars/1.Lab/9. specialNumbers.js
+++ Other Challenges/Fundamentals/2.Data Types and Vars/1.Lab/9. specialNumbers.js
@@ -1,8 +1,20 @@
-function specialNumbers(num) {
-  for (let i = 1; i <= num; i++) {
-    let sum = 0;
-    
+function isSpecial(num) {
+  let sumOfDigits = 0;
+  while (num) {
+    sumOfDigits += num % 10;
+    num = Math.floor(num / 10);
+  }
+  return sumOfDigits === 5 || sumOfDigits === 7 || sumOfDigits === 11;
+}
+
+function printSpecialNumbers(n) {
+  for (let i = 1; i <= n; i++) {
+    if (isSpecial(i)) {
+      console.log(`${i} -> True`);
+    } else {
+      console.log(`${i} -> False`)
+    }
   }
 }
 
-specialNumbers(15);
+printSpecialNumbers(15);
